c1395c94ba944c8a03d5e3cbe4ec324249150e33
Martin <martin.descalzi@gmail.com>
2017-01-05 09:56:19 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'msTTS',
+  packages = ['msTTS'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A random test lib',
+  author = 'Martin Descalzi ',
+  author_email = 'martin.descalzi@gmail.com',
+  url = 'https://github.com/descalzi/msTTS', # use the URL to the github repo
+  download_url = 'https://github.com/descalzi/msTTS/tarball/0.1', # I'll explain this in a second
+  keywords = ['microsoft', 'tts'], # arbitrary keywords
+  classifiers = [],
+)
